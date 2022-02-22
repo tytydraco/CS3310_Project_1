1d5b5823c4c326bda52d85987711cbe43a060d49
Tyler Nijmeh <tylernij@gmail.com>
2022-02-22 15:11:40 -0800

    Add sanity matricies
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,37 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    final int[][] sanityA = {
+            {2, 0, -1, 6},
+            {3, 7, 8, 0},
+            {-5, 1, 6, -2},
+            {8, 0, 2, 7}
+    };
 
+    final int[][] sanityB = {
+            {0, 1, 6, 3},
+            {-2, 8, 7, 1},
+            {2, 0, -1, 0},
+            {9, 1, 6, -2}
+    };
+
+    public static void main(String[] args) {
+        
     }
 
     private static int[][] traditional(int[][] a, int[][] b) {
+        /*
+         * Typically, we multiply each ROW of `a` by each COLUMN of `b` (dot product)
+         */
+
+        int[][] result = new int[][] {};
+
+        for (int aRowIdx = 0; aRowIdx < a.length; aRowIdx++) {
+            for (int bColIdx = 0; bColIdx < b.length; bColIdx++) {
+
+            }
+        }
+
         return new int[][] {};
     }
 
