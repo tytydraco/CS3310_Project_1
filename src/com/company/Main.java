151e565afa95ee41642ed6205f8515f43449a5ed
Tyler Nijmeh <tylernij@gmail.com>
2022-02-22 15:05:17 -0800

    Function placeholders
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    private static int[][] traditional(int[][] a, int[][] b) {
+        return new int[][] {};
+    }
+
+    private static int[][] naive(int[][] a, int[][] b) {
+        return new int[][] {};
+    }
+
+    private static int[][] strassens(int[][] a, int[][] b) {
+        return new int[][] {};
     }
 }
