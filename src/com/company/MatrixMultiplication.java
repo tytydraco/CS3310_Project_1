ed748ec73e7578ed9e59dee939c6ef152f276037
Tyler Nijmeh <tylernij@gmail.com>
2022-02-24 17:30:49 -0800

    Struggle
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- src/com/company/MatrixMultiplication.java
+++ src/com/company/MatrixMultiplication.java
@@ -52,30 +52,49 @@ public class MatrixMultiplication {
         return result;
     }
 
-    private int[][] _naive(int startRow, int endRow, int startCol, int endCol) {
-        int size = endRow - startRow;
+    private int[][] combineIntoBigBoy(int[][] c11, int[][] c12, int[][] c21, int[][] c22) {
+        int size = c11.length * 2;
+        int half = c11.length;
+
+        int[][] c = new int[size][size];
+
+        for (int col = 0; col < half; col++) {
+            System.arraycopy(c11[col], 0, c[col], 0, half);
+            System.arraycopy(c12[col], 0, c[col], half + 1, half);
+        }
+
+        for (int col = half + 1; col < size; col++) {
+            System.arraycopy(c21[col], 0, c[col], 0, half);
+            System.arraycopy(c22[col], 0, c[col], half + 1, half);
+        }
+
+        return c;
+    }
+
+    private int[][] _naive(int rowA, int colA, int rowB, int colB) {
+        int size = colA - rowA;
         int half = size / 2;
 
         int[][] c = new int[size][size];
 
-        System.out.println(size);
-        if (size == 2) {
-            c[0][0] = a[startRow][startCol] * b[startRow][startCol] + a[startRow][endCol] * b[endRow][startCol];    // TOP LEFT
-            c[0][1] = a[startRow][startCol] * b[startRow][endCol] + a[startRow][endCol] * b[endRow][endCol];        // TOP RIGHT
-            c[1][0] = a[endRow][startCol] * b[startRow][startCol] + a[endRow][endCol] * b[endRow][startCol];        // BOT LEFT
-            c[1][1] = a[endRow][startCol] * b[startRow][endCol] + a[endRow][endCol] * b[endRow][endCol];            // BOT RIGHT
+        if (size == 1) {
+            c[0][0] = a[rowA][rowB] * b[rowB][colB];
+            return c;
         } else {
             int[][] c11 = _naive(0, half, 0, half);                   // TOP LEFT
             int[][] c12 = _naive(half + 1, size, 0, half);            // TOP RIGHT
             int[][] c21 = _naive(0, half, half + 1, size);            // BOT LEFT
             int[][] c22 = _naive(half + 1, size, half + 1, size);     // BOT RIGHT
 
+            int[][] g = combineIntoBigBoy(c11, c12, c21, c22);
+            System.out.println(Arrays.deepToString(g));
+
             // TODO: combine into big boy
             // TODO: fix looping forever
             // TODO: ughhhhh
-        }
 
-        return c;
+            return g;
+        }
     }
 
     /**
